b8f61303b41d042bd9a80b5abca7d5b6b8c2828f
Nicolas Muñoz <pepehongomunoz@gmail.com>
2025-12-29 14:37:17 -0300

    fix firebase config

--- frontend/assets/js/firebase-config.js
+++ frontend/assets/js/firebase-config.js
@@ -1,18 +1,18 @@
 // Firebase Configuration - Shared across all pages
 const firebaseConfig = {
-    apiKey: "AIzaSyC_euDpEVl0zL529fl8wFJNEh9Y6Bmbz90",
-    authDomain: "pruebas-de-integracion-1db40.firebaseapp.com",
-    projectId: "pruebas-de-integracion-1db40",
-    storageBucket: "pruebas-de-integracion-1db40.firebasestorage.app",
-    messagingSenderId: "243664775364",
-    appId: "1:243664775364:web:3efe0f8f0e2200b4d54191",
-    measurementId: "G-NZX0Q8VZF3"
+    apiKey: "AIzaSyCB6IL73tSaHKRBtGz3InzRUeWKw3BYLQw",
+    authDomain: "colegio-san-martin-bbf97.firebaseapp.com",
+    projectId: "colegio-san-martin-bbf97",
+    storageBucket: "colegio-san-martin-bbf97.firebasestorage.app",
+    messagingSenderId: "279400417308",
+    appId: "1:279400417308:web:5138e34868e7d1dcae77d9",
+    measurementId: "G-DFM8WTE893"
 };
 
 // Cloudinary Configuration
 const cloudinaryConfig = {
-    cloudName: "doisqyddq",
-    uploadPreset: "Galeria_Prueba"
+    cloudName: "dkqikk5qm",
+    uploadPreset: "ColegioSanMartin"
 };
 
 export { firebaseConfig, cloudinaryConfig };
